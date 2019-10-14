2b80baff54befcfdae721a8854db43561c59fdea
Ingenio1985 <facundo.tello@davinci.edu.ar>
2019-10-14 20:20:50 -0300

    Emproligity

--- Assets/Editor/NodeEditor.cs
+++ Assets/Editor/NodeEditor.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-
 using UnityEngine.SceneManagement;
+using UnityEditor;
 using UnityEditor.SceneManagement;
-
-using System;
-using System.IO;
-
 
 public enum OPTIONS
 {
@@ -17,180 +14,60 @@ public enum OPTIONS
     TREE = 2
 }
 
-
-
-
 public class NodeEditor : EditorWindow
 {
+    string _mName;
+    List<BaseNode> _mNodes;
+    string _nodeName;
 
-    private List<BaseNode> allNodes = new List<BaseNode>();
-    private List<NodeTranslate> allNodesTrans = new List<NodeTranslate>();
-    private NodeTranslateCollection nodeCol ;
-    private GUIStyle myStyle;
-    private INode nodeAux;
-    private GameObject levelAux;
-    private float toolbarHeight = 100;
+    BaseNode _selectedNode;
 
-    private BaseNode _selectedNode;
+    public OPTIONS op; // MUY POSIBLEMENTE LO HAGA VOLAR
+    string toLink = ""; // REVISAR COMO CORREGIR ESTO
 
-    //para el paneo
+    // A REVISAR POR PANEO
     private bool _panningScreen;
     private Vector2 graphPan;
     private Vector2 _originalMousePosition;
     private Vector2 prevPan;
     private Rect graphRect;
 
+    // A REVISAR POR DISEÑO
+    private float toolbarHeight = 100;
     public GUIStyle wrapTextFieldStyle;
-    public OPTIONS op;
-    //LevelManager LvlMan;
 
-    GameObject go;
-
-    private string name = "";
-    private string n = "";
-
-    public string data;
-    public string path;
-
-    [MenuItem("InTello/IaNoder")]
-    //[CustomEditor(typeof(LevelManager)), CanEditMultipleObjects]
+    [MenuItem("InTello/AIcuña")]
     public static void OpenWindow()
     {
-        
-
         var mySelf = GetWindow<NodeEditor>();
-        //mySelf.allNodes = new List<BaseNode>();
-        mySelf.myStyle = new GUIStyle();
-        mySelf.myStyle.fontSize = 20;
-        mySelf.myStyle.alignment = TextAnchor.MiddleCenter;
-        mySelf.myStyle.fontStyle = FontStyle.BoldAndItalic;
-
         mySelf.graphPan = new Vector2(0, mySelf.toolbarHeight);
         mySelf.graphRect = new Rect(0, mySelf.toolbarHeight, 1000000, 1000000);
 
-        //creo un style para asignar a los textos de manera que usen wordwrap
-        //le paso el style por defecto como parametro para mantener el mismo "look"
         mySelf.wrapTextFieldStyle = new GUIStyle(EditorStyles.textField);
         mySelf.wrapTextFieldStyle.wordWrap = true;
-
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
-        //go = GameObject.Find("GameManager");
-        //if (go != null)
-        //{
-        //    LvlMan = go.GetComponent<LevelManager>();
-        //    if (LvlMan == null)
-        //    {
-        //        LvlMan = go.AddComponent<LevelManager>();
-        //    }
-        //}
-        //else
-        //{
-        //    go = new GameObject();
-        //    go.name = "GameManager";
-        //    LvlMan = go.AddComponent<LevelManager>();
-        //}
-
-        //if (LvlMan.allNodes != null && LvlMan.allNodes.Count > 0)
-        //{
-        //    allNodes = LvlMan.allNodes;
-        //    Debug.Log("muestro");
-        //}
-        //serializedObject = new SerializedObject(LvlMan);
-        //LoadData();
-
+        _mName = "";
+        _mNodes = new List<BaseNode>();
     }
-    private void OnGUI()
+
+    void OnGUI()
     {
         CheckMouseInput(Event.current);
-        EditorGUILayout.BeginVertical(GUILayout.Height(100));
-        EditorGUILayout.LabelField("Ia Editor", myStyle, GUILayout.Height(50));
-        EditorGUILayout.Space();
-        EditorGUILayout.BeginHorizontal();
 
-        EditorGUIUtility.labelWidth = 30;
-        EditorGUIUtility.fieldWidth = 20;
-        //nodeAux = (INode)EditorGUILayout.(nodeAux, typeof(INode), true);
-        op = (OPTIONS)EditorGUILayout.EnumPopup("Primitive to create:", op);
-        name = EditorGUILayout.TextField("Node Name",name);
-        if (GUILayout.Button("Add", GUILayout.Width(50), GUILayout.Height(25)))
-        {
-            if (name != "")
-            {
-                AddNode();
+        DrawToolbar();
+        DrawWorkSpace();
 
-
-            }else
-            {
-                EditorUtility.DisplayDialog("Error", "you should name your node","Ok");
-            }
-        }
-
-
-        if (GUILayout.Button("Save", GUILayout.Width(50), GUILayout.Height(25)))
-        {
-            SaveData();
-        }
-
-        EditorGUILayout.Space();
-
-        EditorGUILayout.EndHorizontal();
-        EditorGUILayout.EndVertical();
-
-
-        graphRect.x = graphPan.x;
-        graphRect.y = graphPan.y;
-        EditorGUI.DrawRect(new Rect(0, toolbarHeight, position.width, position.height - toolbarHeight), Color.gray);
-
-        GUI.BeginGroup(graphRect);
-
-        BeginWindows();
-        var oriCol = GUI.backgroundColor;
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-            foreach (var c in allNodes[i].connected)
-            {
-                Handles.DrawLine(new Vector2(allNodes[i].myRect.position.x + allNodes[i].myRect.width / 2f, allNodes[i].myRect.position.y + allNodes[i].myRect.height / 2f), new Vector2(c.myRect.position.x + c.myRect.width / 2f, c.myRect.position.y + c.myRect.height / 2f));
-            }
-        }
-
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-
-
-            allNodes[i].myRect = GUI.Window(i, allNodes[i].myRect, DrawNode, allNodes[i].name);
-
-            //switch (allNodes[i].type)
-            //{
-            //    case 0:
-            //        GUI.backgroundColor = Color.yellow;
-            //        break;
-            //    case 1:
-            //        GUI.backgroundColor = Color.red;
-            //        break;
-            //    default:
-            //        GUI.backgroundColor = Color.blue;
-            //        break;
-            //}
-
-            if (allNodes[i] == _selectedNode)
-                GUI.backgroundColor = Color.grey;
-
-        }
-
-        EndWindows();
-        GUI.EndGroup();
     }
 
-
-    private void CheckMouseInput(Event currentE)
+    // REVISAR PARA EL PANEO
+    void CheckMouseInput(Event currentE)
     {
         if (!graphRect.Contains(currentE.mousePosition) || !(focusedWindow == this || mouseOverWindow == this))
             return;
 
-        //panning
         if (currentE.button == 2 && currentE.type == EventType.MouseDown)
         {
             _panningScreen = true;
@@ -211,16 +88,13 @@ public class NodeEditor : EditorWindow
             Repaint();
         }
 
-
         BaseNode overNode = null;
-        for (int i = 0; i < allNodes.Count; i++)
+        for (int i = 0; i < _mNodes.Count; i++)
         {
-            allNodes[i].CheckMouse(Event.current, graphPan);
-            if (allNodes[i].OverNode)
-                overNode = allNodes[i];
+            _mNodes[i].CheckMouse(Event.current, graphPan);
+            if (_mNodes[i].OverNode)
+                overNode = _mNodes[i];
         }
-
-
 
         var prevSel = _selectedNode;
         if (currentE.button == 0 && currentE.type == EventType.MouseDown)
@@ -235,220 +109,166 @@ public class NodeEditor : EditorWindow
         }
     }
 
-
-    private void AddNode()
+    /// <summary>
+    /// Dibuja la barra de herramientas.
+    /// </summary>
+    void DrawToolbar()
     {
-        allNodes.Add(new BaseNode(0, 0, 200, 150, (int)op, name));
-        name = "";
-        Repaint();
-
-    }
-
-
-
-    private void DrawNode(int id)
-    {
-
-        //le dibujamos lo que queramos al nodo...
-        EditorGUILayout.BeginHorizontal();
-        //EditorGUILayout.LabelField("Dialogo", GUILayout.Width(100));
-
-        //allNodes[id].name = EditorGUILayout.TextField(allNodes[id].name, wrapTextFieldStyle, GUILayout.Height(50));
-        EditorGUILayout.EndHorizontal();
-        //EditorGUILayout.FloatField("Id ", allNodes[id].Id);
-
-        Repaint();
-
-        n = EditorGUILayout.TextField("Link",n);
-        if (allNodes[id].type != 0 )
+        EditorGUILayout.BeginVertical(GUILayout.Height(100));
         {
-            if((allNodes[id].type == 1 && allNodes[id].connected.Count <= 2)) {
+            EditorGUILayout.BeginHorizontal();
+            {
+                _mName = EditorGUILayout.TextField("Map Name", _mName);
+                if (GUILayout.Button("Save Map"))
+                    SaveMap();
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            {
+                op = (OPTIONS)EditorGUILayout.EnumPopup("Primitive to create:", op);
+                _nodeName = EditorGUILayout.TextField("Node Name", _nodeName);
                 if (GUILayout.Button("Add", GUILayout.Width(50), GUILayout.Height(25)))
                 {
-                    Debug.Log(n);
+                    if (_nodeName != "")
+                        AddNode();
+                    else
+                        EditorUtility.DisplayDialog("Error", "You should name your node.", "Ok. I'm Sorry.");
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
 
-                    for (int i = 0; i < allNodes.Count; i++)
+        }
+        EditorGUILayout.EndVertical();
+
+    }
+
+    /// <summary>
+    /// Salva el mapa de nodos.
+    /// </summary>
+    void SaveMap()
+    {
+
+    }
+
+    /// <summary>
+    /// Agrega un nuevo nodo.
+    /// </summary>
+    private void AddNode()
+    {
+        _mNodes.Add(new BaseNode(0, 0, 200, 150, (int)op, _nodeName));
+        _nodeName = "";
+        Repaint();
+    }
+
+    /// <summary>
+    /// Dibuja el espacio nde trabajo.
+    /// </summary>
+    void DrawWorkSpace()
+    {
+        graphRect.x = graphPan.x;
+        graphRect.y = graphPan.y;
+        EditorGUI.DrawRect(new Rect(0, toolbarHeight, position.width, position.height - toolbarHeight), Color.gray);
+
+        GUI.BeginGroup(graphRect);
+        {
+            BeginWindows();
+            {
+                var defaultColor = GUI.backgroundColor;
+
+                for (int i = 0; i < _mNodes.Count; i++)
+                {
+                    foreach (var n in _mNodes[i].connected)
                     {
-                        if (allNodes[i].name == n)
-                        {
-                            Debug.Log("Entra");
-                            allNodes[id].connected.Add(allNodes[i]);
+                        _mNodes[i].myRect = GUI.Window(i, _mNodes[i].myRect, DrawNode, _mNodes[i].name);
+                        if (_mNodes[i] == _selectedNode)
+                            GUI.backgroundColor = Color.grey;
+                        GUI.backgroundColor = defaultColor;
 
-                            n = "";
-
-                        }
+                        // REVISAR DIBUJO DE LINEAS CONECTORAS
+                        Handles.DrawLine(new Vector2(_mNodes[i].myRect.position.x + _mNodes[i].myRect.width / 2f,
+                            _mNodes[i].myRect.position.y + _mNodes[i].myRect.height / 2f),
+                            new Vector2(n.myRect.position.x + n.myRect.width / 2f,
+                            n.myRect.position.y + n.myRect.height / 2f));
                     }
                 }
-
             }
+            EndWindows();
         }
-        Repaint();
-        //if (n != null)
-        //{
-        //    for (int i = 0; i < allNodes.Count; i++)
-        //    {
-
-        //            //if (allNodes[i].Id != 0)
-        //            //{
-        //            //    level = allNodes[i].level;
-        //            //    AddNode();
-        //            //}
-        //            //allNodes[i].Id = allNodes[id].connected.Count + 1;
-
-        //            //allNodesTrans[id].AddConnect(allNodes[i].level);
-                
-        //    }
-
-        //}
-
-        if (GUILayout.Button("Delete", GUILayout.Width(50), GUILayout.Height(25)))
-        {
-            Remove(id);
-        }
-
-        Repaint();
-
-        if (!_panningScreen)
-        {
-            //esto habilita el arrastre del nodo.
-            //pasandole como parámetro un Rect podemos setear que la zona "agarrable" a una específica.
-            GUI.DragWindow();
-
-            if (!allNodes[id].OverNode) return;
-
-            //clampeamos los valores para asegurarnos que no se puede arrastrar el nodo por fuera del "área" que nosotros podemos panear
-            if (allNodes[id].myRect.x < 0)
-                allNodes[id].myRect.x = 0;
-
-            if (allNodes[id].myRect.y < toolbarHeight - graphPan.y)
-                allNodes[id].myRect.y = toolbarHeight - graphPan.y;
-        }
+        GUI.EndGroup();
     }
 
-    private void Remove(int id)
+    /// <summary>
+    /// Dibuja el nodo.
+    /// </summary>
+    /// <param name="id">ID del nodo a dibujar</param>
+    private void DrawNode(int id)
     {
-        for (int i = 0; i < allNodes.Count; i++)
+        toLink = EditorGUILayout.TextField("Link", toLink);
+        if (_mNodes[id].type != 0)
         {
-            allNodes[i].connected.Remove(allNodes[id]);
-        }
-        allNodes.RemoveAt(id);
-
-    }
-
-
-    #region setWork
-    private void SaveData()
-    {
-
-        string auxCon = "";
-        allNodesTrans.Clear();
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-            for (int ia = 0; ia < allNodes[i].connected.Count; ia++)
+            if ((_mNodes[id].type == 1 && _mNodes[id].connected.Count <= 2))
             {
-                auxCon = allNodes[i].connected[ia].name+ "," + auxCon;
-            }
-            NodeTranslate nd = new NodeTranslate(allNodes[i].name, auxCon, allNodes[i].myRect.x, allNodes[i].myRect.y);
-            allNodesTrans.Add(nd);
-            auxCon = "";
-        }
-
-
-        nodeCol = new NodeTranslateCollection(allNodesTrans);
-        data = JsonUtility.ToJson(nodeCol, true);
-        
-        File.WriteAllText(path, data);
-    }
-
-    private void LoadData()
-    {
-
-        path = Application.persistentDataPath + "/SavedData.json";
-
-        Debug.Log(path);
-
-        if (File.Exists(path))
-        {
-            data = File.ReadAllText(path);
-            nodeCol = JsonUtility.FromJson<NodeTranslateCollection>(data);
-            if (nodeCol != null)
-            {
-                allNodesTrans = nodeCol.allNodes;
-                TranslateNode();
-            }
-        }
-        else
-        {
-            
-            File.WriteAllText(path, "");
-        }
-
-    }
-
-    private void TranslateNode()
-    {
-        for (int i = 0; i < allNodesTrans.Count; i++)
-        {
-            if (GameObject.Find(allNodesTrans[i].level) != null)
-            {
-                AddNode(GameObject.Find(allNodesTrans[i].level), allNodesTrans[i].x, allNodesTrans[i].y);
-
-            }
-
-        }
-
-        for (int i = 0; i < allNodes.Count; i++)
-        {
-
-            foreach (var item in allNodesTrans[i]._connect.Split(','))
-            {
-                if (item != "")
+                if (GUILayout.Button("Connect", GUILayout.Width(50), GUILayout.Height(25)))
                 {
-                    allNodes[i].connected.Add(allNodes.Find(x => x.name == item.ToString()));
-
+                    ConnectNode(id);
                 }
             }
         }
+        if (GUILayout.Button("Delete", GUILayout.Width(50), GUILayout.Height(25)))
+        {
+            RemoveNode(id);
+        }
+
+        // REVISAR PANEO
+        if (!_panningScreen)
+        {
+            GUI.DragWindow();
+
+            if (!_mNodes[id].OverNode) return;
+
+            if (_mNodes[id].myRect.x < 0)
+                _mNodes[id].myRect.x = 0;
+
+            if (_mNodes[id].myRect.y < toolbarHeight - graphPan.y)
+                _mNodes[id].myRect.y = toolbarHeight - graphPan.y;
+        }
+    }
+
+    /// <summary>
+    /// Conecta el nodo con el indicado en el campo.
+    /// </summary>
+    /// <param name="id">ID del nodo a eliminar</param>
+    void ConnectNode(int id)
+    {
+        for (int i = 0; i < _mNodes.Count; i++)
+        {
+            if (_mNodes[i].name == toLink)
+            {
+                _mNodes[id].connected.Add(_mNodes[i]);
+                toLink = "";
+            }
+        }
+
         Repaint();
     }
 
-    private void AddNode(GameObject lvl, float x, float y)
+    /// <summary>
+    /// Elimina un nodo y lo saca de todas las listas.
+    /// </summary>
+    /// <param name="id">ID del nodo a eliminar</param>
+    void RemoveNode(int id)
     {
+        for (int i = 0; i < _mNodes.Count; i++)
+        {
+            _mNodes[i].connected.Remove(_mNodes[id]);
+        }
+        _mNodes.RemoveAt(id);
 
-        //allNodes.Add(new BaseNode(x, y, 200, 150, lvl));
-
-
-        //Repaint();
-
-    }
-    #endregion
-
-}
-public class NodeTranslateCollection
-{
-    public List<NodeTranslate> allNodes;
-
-    public NodeTranslateCollection(List<NodeTranslate> node)
-    {
-        allNodes = node;
+        Repaint();
     }
 }
 
-[Serializable]
-public class NodeTranslate
-{
-    public float x;
-    public float y;
-    public string level;
-    public string _connect;
 
-    public NodeTranslate(string _level, string connect, float _x, float _y)
-    {
-        x = _x;
-        y = _y;
-        level = _level;
-        _connect = connect;
-    }
-
-
-}
---
+++ Assets/Script/AIcuñaMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CreateAssetMenu(fileName = "New AIcuñaMap", menuName = "AIcuña Map", order = 51)]
+public class AIcuñaMap : ScriptableObject
+{
+    [SerializeField]
+    string name;
+    //[SerializeField]
+    //List<BaseNode> nodes;
+
+}
