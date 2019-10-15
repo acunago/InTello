f19560766b3638f6ba129609eab828066d559455
acunago <49562365+acunago@users.noreply.github.com>
2019-10-15 00:36:01 -0300

    Asi nomas

---
+++ Assets/MuestraScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MuestraScript : MonoBehaviour
+{
+    public UnityEvent onEvent;
+    public MyCondition cond;
+    bool myResult;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onEvent.Invoke();
+        myResult = cond.Invoke();
+        Debug.Log("El resultado es" + myResult);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+[Serializable]
+public class MyCondition : SerializableCallback<bool> { }
---
+++ Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public bool ReturnTrue()
+    {
+        return true;
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Ejecuto sin parametros");
+    }
+}
